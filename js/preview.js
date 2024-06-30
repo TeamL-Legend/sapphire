7bd71913f9ae06f8d5d8dad4d2c4b629efb1f179
TeamL-Legend <NaNLegendN1@gmail.com>
2024-06-30 23:46:00 +0500

    Add files via upload

---
+++ js/preview.js
@@ -0,0 +1,5 @@
+let nextbtnNode = document.querySelector(`#nextbtn`);
+nextbtnNode.addEventListener(`click`, function(){
+    let noneNode = document.querySelector(`#none`);
+    noneNode.classList.remove(`none`);
+});
